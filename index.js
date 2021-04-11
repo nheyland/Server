f95141e7fd394a512fd12960d97b0c1957c97e31
Nick Heyland <nheyland@gmail.com>
2021-04-10 20:27:16 -0400

    tests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const port = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 5000;
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -8,6 +9,22 @@ const login = require('./routes/secure/login')
 
 const a = require('./routes/content/a')
 const b = require('./routes/content/b')
+
+
+
+
+const cool = require('cool-ascii-faces');
+const path = require('path');
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
 
 
 dotenv.config()
@@ -25,5 +42,5 @@ app.use('/b', b)
 
 
 
-app.listen(port, () => console.log('Server Running....'))
+app.listen(PORT, () => console.log('Server Running....'))
 
