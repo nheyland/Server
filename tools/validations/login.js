55f7d94e0462f6819b029bb7de7299bbd20ab2ad
Nick Heyland <nheyland@gmail.com>
2021-04-10 22:07:02 -0400

    fix

--- tools/validations/login.js
+++ tools/validations/login.js
@@ -1,10 +1,12 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
-const loginValidation = data => {
-    const schema = Joi.object({
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    });
-    return schema.validate(data.body);
-}
-module.exports.loginValidation = loginValidation
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email().label("Email!"),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+  });
+  return schema.validate(data.body);
+};
+module.exports.loginValidation = loginValidation;
